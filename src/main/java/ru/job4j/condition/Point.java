1b90fa8dec979009b51ec7e4e1eecd85f2a7c2e6
aleksandrpozdnyakov <mr.pv.podnekov@yandex.ru>
2024-11-15 06:15:15 +0300

    7. Расстояние между точками в системе координат [#188 #521698]. Убран ненужный импорт.

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,7 +1,5 @@
 package ru.job4j.condition;
 
-import com.sun.jdi.PathSearchingVirtualMachine;
-
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
         double resualt = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
